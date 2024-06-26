eecf2a1faa7d6811dffe0dfcc9d124e02801cf13
chutoan2306 <truongvanchutoan1999@gmail.com>
2024-06-26 14:40:52 +0700

    chore:merge code

--- src/systems/other/baseMode.system.ts
+++ src/systems/other/baseMode.system.ts
@@ -1,34 +1,6 @@
-export default class BaseModel {
-	private _is_active: boolean = false
-	private _createdAt: string = new Date().toISOString()
-	private _updatedAt: string = new Date().toISOString()
-	private _deleteAt?: null
-
-	get is_active(): boolean {
-		return this._is_active
-	}
-	set is_active(value: boolean) {
-		this._is_active = value
-	}
-
-	get createdAt(): string {
-		return this._createdAt
-	}
-	set createdAt(value: string) {
-		this._createdAt = value
-	}
-
-	get updatedAt(): string {
-		return this._updatedAt
-	}
-	set updatedAt(value: string) {
-		this._updatedAt = value
-	}
-
-	get deleteAt(): null | undefined {
-		return this._deleteAt
-	}
-	set deleteAt(value: null | undefined) {
-		this._deleteAt = value
-	}
+export default interface BaseModel {
+	is_active: boolean
+	createdAt: string
+	updatedAt: string
+	deleteAt?: null
 }
